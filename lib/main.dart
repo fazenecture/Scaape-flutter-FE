6bc96168b94e65df29dd7f6f5b8b411c3555cfcc
chakravarthi-v <chakravarthiviswanath@gmail.com>
2021-10-05 18:20:56 +0530

    feat: chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,7 @@ class _MyAppState extends State<MyApp> {
           } else {
             return OnBoarding();
            }
+          //return OnBoarding();
           },
       ),
       routes: {
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:scaape/screens/usersChat.dart';
 
 class ScaapeChat extends StatefulWidget {
@@ -20,54 +24,88 @@ class _ScaapeChatState extends State<ScaapeChat> {
 
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    String authId=auth.currentUser!.uid;
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Container(
-          color: Color(0xFF222831),
-          margin: EdgeInsets.fromLTRB(15, 58, 15, 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+      child: FutureBuilder(
+        future: getActiveScaapes(authId),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if(snapshot.hasData){
+            var a=snapshot.data;
+
+            return SingleChildScrollView(
+              child: Container(
+                color: Color(0xFF222831),
+                margin: EdgeInsets.fromLTRB(15, 58, 15, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Icon(
-                      Icons.notification_important_outlined,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      'Social Messages',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 26,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.notification_important_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Social Messages',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 26,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
+                    ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: Color(0xFFFF4B2B).withOpacity(0.5),
+                              radius: 22,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                backgroundImage: NetworkImage(a[index]['ScaapeImg']),
+                                radius: 19,
+                              ),
+                            ),
+                            title: Text(a[index]['ScaapeName']),
+                            subtitle: Text("4 Messages"),
+                            onTap: () {
+                              Navigator.pushNamed(context, UserChat.id);
+                            },
+                          );
+                        }),
                   ],
                 ),
               ),
-              ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: Icon(Icons.add_a_photo_sharp),
-                      title: Text("hello"),
-                      subtitle: Text("4 Messages"),
-                      onTap: () {
-                        Navigator.pushNamed(context, UserChat.id);
-                      },
-                    );
-                  }),
-            ],
-          ),
-        ),
+            );
+          }
+          else{
+            return Center(child: CircularProgressIndicator(),);
+          }
+        },
+
       ),
     );
   }
+}
+Future<List<dynamic>> getActiveScaapes(String id)async{
+
+  String url='http://65.0.121.93:4000/api/getScaapesById/UserId=2/Status=test';
+
+  Response response=await get(Uri.parse(url));
+  int statusCode = response.statusCode;
+  print(statusCode);
+  print(response.body);
+  print(json.decode(response.body));
+  return json.decode(response.body);
 }
