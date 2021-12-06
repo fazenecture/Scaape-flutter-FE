2d820ecca32ea537c41c854fefd75b4847d7f516
Vivek <37245253+fazenecture@users.noreply.github.com>
2021-12-07 00:28:10 +0530

    map-screen

---
+++ lib/utils/scaapeData.dart
@@ -0,0 +1,15 @@
+
+
+
+
+class Scaapedata{
+  double lat;
+  double lon;
+  String imgURL;
+  String sName;
+  String sLocation;
+
+  Scaapedata({required this.lat,required this.lon,required this.imgURL, required this.sLocation, required this.sName});
+
+
+}
