b40b47475fd944968b2f06db4a1e6e90bf8a39ec
Vivek <37245253+fazenecture@users.noreply.github.com>
2021-12-07 01:12:52 +0530

    map-button-fix

--- lib/screens/explore_page.dart
+++ lib/screens/explore_page.dart
@@ -13,8 +13,6 @@ import 'package:fluster/src/base_cluster.dart';
 import 'package:fluster/src/clusterable.dart';
 import 'package:scaape/utils/scaapeData.dart';
 
-
-
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
   static String id = 'explorePage';
@@ -24,7 +22,6 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
-
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   late GoogleMapController newGoogleMapController;
 
@@ -39,15 +36,19 @@ class _ExplorePageState extends State<ExplorePage> {
   late double longitude;
 
   @override
-  void initState(){
+  void initState() {
     scaapesLocator();
   }
 
   // Map<double,double> scaapeList ={};
   List<Scaapedata> scaapeList = [];
-  Future<List<Scaapedata>> scaapesLocator() async{
+
+
+
+
+  Future<List<Scaapedata>> scaapesLocator() async {
     String url;
-    url ='https://api.scaape.online/api/getScaapes';
+    url = 'https://api.scaape.online/api/getScaapes';
     print(url);
     http.Response response = await http.get(Uri.parse(url));
     var data = json.decode(response.body);
@@ -55,7 +56,7 @@ class _ExplorePageState extends State<ExplorePage> {
     print("htis i lens");
     print(len);
     setState(() {
-      for(int i = 0; i < len ; i++){
+      for (int i = 0; i < len; i++) {
         double lat = data[i]['Lat'];
         double long = data[i]['Lng'];
         String img = data[i]['ScaapeImg'];
@@ -64,65 +65,65 @@ class _ExplorePageState extends State<ExplorePage> {
         print(i);
         // print(lat.toString);
         // scaapeList[lat] = long;
-        scaapeList.add(Scaapedata(lat: lat, lon: long, imgURL:img, sLocation: scaapeLocation, sName: scaapeName ));
+        scaapeList.add(Scaapedata(
+            lat: lat,
+            lon: long,
+            imgURL: img,
+            sLocation: scaapeLocation,
+            sName: scaapeName));
 
         // print(scaapeList[lat]);
         // scaapeList.add(data);
       }
     });
 
-
     // scaapeList[17.3850] = 78.4867;
-  print("this is ");
-  // print(scaapeList);
-
+    print("this is ");
+    // print(scaapeList);
 
     // print(data);
     getmarkers();
     return scaapeList;
-
-
   }
 
-
-
-
-  void locatePosition () async{
-
-    try{
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  void locatePosition() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
       laitutde = position.latitude;
       longitude = position.longitude;
 
       LatLng latLaPosition = LatLng(position.latitude, position.longitude);
 
-
-      CameraPosition cameraPosition = new CameraPosition(target: latLaPosition, zoom: 14);
-      newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+      CameraPosition cameraPosition =
+          new CameraPosition(target: latLaPosition, zoom: 14);
+      newGoogleMapController
+          .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
 
       print(longitude);
       print(laitutde);
-    }
-    catch(e){
+    } catch (e) {
       print(e);
       print('kuch problem h');
     }
   }
 
-
   void setMap(String mapStyle) {
-  newGoogleMapController.setMapStyle(mapStyle);
+    newGoogleMapController.setMapStyle(mapStyle);
   }
-  void loadMap(){
-     getJsonFile('mapJson/nigh.json').then(setMap);
+
+  void loadMap() {
+    getJsonFile('mapJson/nigh.json').then(setMap);
   }
-  Future<String> getJsonFile(String path) async{
+
+  Future<String> getJsonFile(String path) async {
     return await rootBundle.loadString(path);
   }
+
   final Set<Marker> markers = new Set();
 
-
   static const currentZoom = 10;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -136,68 +137,59 @@ class _ExplorePageState extends State<ExplorePage> {
               mapType: MapType.normal,
               myLocationEnabled: true,
               myLocationButtonEnabled: true,
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.5),
+              padding: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height * 0.06),
               onMapCreated: (GoogleMapController controller) {
                 _controllerGoogleMap.complete(controller);
                 newGoogleMapController = controller;
                 loadMap();
                 locatePosition();
               },
-              onLongPress: (value){
+              onLongPress: (value) {
                 print(value);
               },
               markers: markers.map((e) => e).toSet(),
             ),
-
-
-
           ],
         ),
       ),
-
-
-
-
     );
   }
+
   // Future<Uint8List> getUint8List(var image) async {
   //
   //   // ByteData byteData = await image.;
   //   // return byteData.buffer.asUint8List();
   // }
-  Future<void> getmarkers() async { //markers to place on map
+  Future<void> getmarkers() async {
+    //markers to place on map
     int cnt = 0;
 
-    for(Scaapedata data in scaapeList){
+    for (Scaapedata data in scaapeList) {
       final http.Response response = await http.get(Uri.parse(data.imgURL));
 
-        cnt++;
-        print('This is image URL${data.imgURL}');
-        // print("this is key $key");
-        // print("this is value $value");
-        markers.add(Marker( //add first marker
-            markerId: MarkerId(cnt.toString()),
-            position: LatLng(data.lat, data.lon),
-            infoWindow: InfoWindow(
-              title: data.sName,
-              snippet: data.sLocation,
-            ),
-
-            icon: BitmapDescriptor.fromBytes(response.bodyBytes)//Icon for Marker
-        ));
-
-
+      cnt++;
+      print('This is image URL${data.imgURL}');
+      // print("this is key $key");
+      // print("this is value $value");
+      markers.add(Marker(
+          //add first marker
+          markerId: MarkerId(cnt.toString()),
+          position: LatLng(data.lat, data.lon),
+          infoWindow: InfoWindow(
+            title: data.sName,
+            snippet: data.sLocation,
+          ),
+          icon: BitmapDescriptor.fromBytes(response.bodyBytes) //Icon for Marker
+          ));
     }
     setState(() {
       print("list of marker is $markers");
-
     });
     // scaapeList.forEach((key, value) {
     //
     // });
     //add more markers here
-
-
   }
 }
 
@@ -207,22 +199,20 @@ class MapMarker extends Clusterable {
 
   MapMarker(
       {this.locationName,
-        latitude,
-        longitude,
-        this.thumbnailSrc,
-        isCluster = false,
-        clusterId,
-        pointsSize,
-        markerId,
-        childMarkerId})
+      latitude,
+      longitude,
+      this.thumbnailSrc,
+      isCluster = false,
+      clusterId,
+      pointsSize,
+      markerId,
+      childMarkerId})
       : super(
-      latitude: latitude,
-      longitude: longitude,
-      isCluster: isCluster,
-      clusterId: clusterId,
-      pointsSize: pointsSize,
-      markerId: markerId,
-      childMarkerId: childMarkerId);
+            latitude: latitude,
+            longitude: longitude,
+            isCluster: isCluster,
+            clusterId: clusterId,
+            pointsSize: pointsSize,
+            markerId: markerId,
+            childMarkerId: childMarkerId);
 }
-
-
